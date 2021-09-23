71319fc300eae209cdaad0e8a03666a7ba755260
ozerova daria <ozerova.daria2016@mail.ru>
2021-09-23 16:56:10 +0300

    3 задача 23/09/21

---
+++ 20210923/3/prog.py
@@ -0,0 +1,18 @@
+def sum_of_ch(x):
+    sum = 0
+    while x > 0:
+        sum += x % 10
+        x //= 10
+    return sum
+n = int(input())
+i = j = n
+while i != n+3:
+    while j != n+3:
+        if sum_of_ch(i*j) == 6:
+            print (":=)", end = " ")
+        else:
+            print (i,"*",j, "=", i * j, end = " ")
+        j += 1
+    j = n
+    i += 1
+    print('\n')
