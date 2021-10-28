49b7f562e4a48701917b0cda1ba1b2be15f3d4c7
ozerova daria <ozerova.daria2016@mail.ru>
2021-10-28 20:38:10 +0300

    3 задачка

--- 20211028/3/prog.py
+++ 20211028/3/prog.py
@@ -1,6 +1,9 @@
 import itertools
+
 """
 print(list(itertools.product("qwer", "123")))
 print(list(itertools.permutations("qwer", 2)))
 """
-print(list(itertools.filter(int(input()))))
+
+print(*sorted(filter(lambda x: x.count("TOR") == 2 , map(lambda x: "".join(x).replace('*', 'TOR') , itertools.product('TOR*', repeat = int(input()) - 4))))) 
+
