fec94e133b34508908f433fbe09aa31dc7fc3df2
ozerova daria <ozerova.daria2016@mail.ru>
2021-11-05 01:26:08 +0300

    solved

--- 20211014/1/prog.py
+++ 20211014/1/prog.py
@@ -1,29 +1,25 @@
 import fractions
-def make(m, grade):
-    sum = 0
-    res = 0
-    i = 3
-    while (grade >=0):
-        sum += m[0]**(grade)* m[i]
-        i += 1
-        grade -= 1
-    i += 1
-    grade = m[i-1]
-    while (grade >=0):
-        sum += m[0]**(grade)* m[i]
-        i += 1
-        grade -= 1
-    if res != 0:
-        return(sum / res == m[1])
-    else: return(False)
-
 str = input().split(", ")
 m = []
 for i in str:
     m.append(fractions.Fraction(i))
-if make(m, m[2]):
-    print("True")
-else:
-    print("False")
-
-
+res = 0
+sum = 0
+i = 3
+grade = m[2]
+while (grade >=0):
+    sum += m[0]**(grade)* m[i]
+    i += 1
+    grade -= 1
+i += 1
+grade = m[i-1]
+while (grade >=0):
+    res += m[0]**(grade)* m[i]
+    i += 1
+    grade -= 1
+if res != 0:
+    if (sum / res == m[1]):
+        print('True')
+    else:
+        print('False')
+else: print('False')
