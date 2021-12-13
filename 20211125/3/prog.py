cc07fc87f71692683ac7d334e1f94298e1a7c3de
ozerova daria <ozerova.daria2016@mail.ru>
2021-12-13 08:58:38 +0300

    3 ts

--- 20211125/3/prog.py
+++ 20211125/3/prog.py
@@ -1,1 +1,10 @@
-prog3
+#prog3
+import sys
+st = sys.stdin.buffer.read()
+a = int.from_bytes(st[4:8],   'little')
+b = int.from_bytes(st[20:22], 'little')
+c = int.from_bytes(st[22:24], 'little')
+d = int.from_bytes(st[24:28], 'little')
+e = int.from_bytes(st[34:36], 'little')
+f = int.from_bytes(st[40:44], 'little')
+print(f'Size={a}, Type={b}, Channels={c}, Rate={d}, Bits={e}, Data size={f}')
