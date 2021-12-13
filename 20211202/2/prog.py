977ed743845a741c0bcb94c8f9cd6c7f64c91980
ozerova daria <ozerova.daria2016@mail.ru>
2021-12-13 14:11:52 +0300

    new

--- 20211202/2/prog.py
+++ 20211202/2/prog.py
@@ -1,9 +1,10 @@
 class check(type):
-    def __init__(cls, cls_name, parents, ns):
+    def __init__(cls, n, father, ns):
         def checkit(self):
             for (n, _type) in self.__annotations__.items():
-                my_aatr = getattr(self, n, None)
-                if not isinstance(my_aatr, _type):
+                if isinstance(getattr(self, n, None), _type):
+                    continue
+                else:
                     return False
             return True
         cls.check_annotations = checkit
