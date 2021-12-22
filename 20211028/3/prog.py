4c6b98419dd32f03d963f77426e2058074edc138
ozerova daria <ozerova.daria2016@mail.ru>
2021-12-22 22:20:46 +0300

    commit

--- 20211028/3/prog.py
+++ 20211028/3/prog.py
@@ -5,7 +5,7 @@ print(list(itertools.product("qwer", "123")))
 print(list(itertools.permutations("qwer", 2)))
 """
 
-print(*sorted(filter(lambda x: x.count("TOR") == 2 , map(lambda x: "".join(x).replace('*', 'TOR') , itertools.product('TOR*', repeat = int(input()) - 4))))) 
+print(*sorted(filter(lambda x: x.count("TOR") == 2 , map(lambda x: "".join(x).replace('*', 'TOR') , itertools.product('TOR*', repeat = int(input()) - 4)))), sep=', ') 
 
 import sys
 exec(sys.stdin.read())
