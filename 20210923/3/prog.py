09f5d3d7e1e9f39a760889ee55214eb47673099d
ozerova daria <ozerova.daria2016@mail.ru>
2021-12-22 21:06:04 +0300

    commit

--- 20210923/3/prog.py
+++ 20210923/3/prog.py
@@ -9,10 +9,10 @@ i = j = n
 while i != n+3:
     while j != n+3:
         if sum_of_ch(i*j) == 6:
-            print (i,"*",j, "=", ":=)", sep='', end = "")
+            print (i,"*",j, "=", ":=)", sep='', end = " ")
         else:
-            print (i,"*",j, "=", i * j, sep='', end = "")
+            print (i,"*",j, "=", i * j, sep='', end = " ")
         j += 1
     j = n
     i += 1
-    print('\n')
+    #print('\n')
