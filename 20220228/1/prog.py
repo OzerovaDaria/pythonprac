c814606dd6fae918cd38f5a87db325738360485b
ozerova daria <ozerova.daria2016@mail.ru>
2022-03-03 23:25:41 +0300

    1/5

---
+++ 20220228/1/prog.py
@@ -0,0 +1,10 @@
+import multiprocessing as mp
+import textdistance
+
+def dist(s1, s2, m):
+    if m == "D":
+        return textdistance.damerau_levenshtein(s1, s2)
+    elif m == "L":
+        return textdistance.levenshtein(s1, s2)
+    else:
+        return -1
