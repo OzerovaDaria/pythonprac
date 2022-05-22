751e46a7aee7c706f549d4fa5234c9b8455009c2
ozerova daria <ozerova.daria2016@mail.ru>
2022-05-22 23:19:29 +0300

    hometask

--- 20220411/1/prog.py
+++ 20220411/1/prog.py
@@ -1,9 +1,10 @@
 import gettext
 
-translation = gettext.translation('prog', 'po', fallback=True)
+translation = gettext.translation('messages', 'po', fallback=True)
 _, ngettext = translation.gettext, translation.ngettext
 
 def dialog():
+
     while s := input(_("Input a string: ")):
         n = len(s.split())
         print(ngettext('Entered {} word', 'Entered {} words', n).format(n))
