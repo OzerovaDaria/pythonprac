14f168420a6bf90eadc8fa65b2f4e2795d902e66
ozerova daria <ozerova.daria2016@mail.ru>
2022-05-26 00:30:26 +0300

    2/3

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,8 +1,8 @@
 
 import textdistance as td
 
-def dist(s1, s2):
+def dist(s1, s2, s3):
     return td.levenshtein(s1, s2)
 
 a, b, c = input(), input(), input()
-res = dist(a, b)
+res = dist(a, b, c)
